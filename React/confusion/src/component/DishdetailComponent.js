9707a79284803052fabcd42c6295f9db394ad8b0
Meet Doshi <meetdoshi193@gmail.com>
2020-06-30 23:37:54 +0530

    Single Page Applications Part 2

--- React/confusion/src/component/DishdetailComponent.js
+++ React/confusion/src/component/DishdetailComponent.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
         if (dish != null) {
             return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
             );
         }
         else {
@@ -20,26 +23,21 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         }
     }
 
-    function RenderComments({dish}) {
-        if (dish != null && dish.comments != null) {
-            const commentList = dish.comments.map((c) => {
-                return(
-                    <div key={c.id}>
-                        <li>
-                            {c.comment}
-                        </li><br/>
-                        <li>
-                            -- {c.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(c.date)))}
-                        </li><br/>
-                    </div>
-                );
-            });
+    function RenderComments({comments}) {
+        if (comments != null) {
 
             return(
-                <div>
+                <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                     <ul className="list-unstyled">
-                        {commentList}
+                        {comments.map((c) => {
+                            return(
+                                <li>
+                                    <p>{c.comment}</p>
+                                    <p>-- {c.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(c.date)))}</p>
+                                </li>
+                            );
+                        })}
                     </ul>
                 </div>
             );
@@ -55,12 +53,18 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         return(
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
                     </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments dish={props.dish} />
-                    </div>
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         );
--- React/confusion/src/component/MainComponent.js
+++ React/confusion/src/component/MainComponent.js
@@ -33,14 +33,22 @@ class Main extends Component {
             );
         }
 
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                  comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+            );
+        }
+
         return(
             <div>
                 <Header />
                 <Switch>
-                    <Route path="/home" component={HomePage} />
-                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> } />
-                    <Route exact path="/contactus" component={Contact} />
-                    <Redirect path="/home" />
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} /> } />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
+                    <Redirect path='/home' />
                 </Switch>
                 <Footer />
             </div>
