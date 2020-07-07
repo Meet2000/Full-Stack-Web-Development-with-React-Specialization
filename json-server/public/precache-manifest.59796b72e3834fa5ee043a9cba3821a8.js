5832ded306bfda3de691521f012d4f450d30f937
Meet Doshi <meetdoshi193@gmail.com>
2020-07-07 21:33:08 +0530

    Completed React

---
+++ json-server/public/precache-manifest.59796b72e3834fa5ee043a9cba3821a8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957234b2eff3c70def8ec1e11c20c63d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6e7cfb2b5afdfe2827b",
+    "url": "/static/css/2.c21e1fb8.chunk.css"
+  },
+  {
+    "revision": "f8599cffa40cc8d70393",
+    "url": "/static/css/main.8d389e83.chunk.css"
+  },
+  {
+    "revision": "c6e7cfb2b5afdfe2827b",
+    "url": "/static/js/2.18cf85a8.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.18cf85a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8599cffa40cc8d70393",
+    "url": "/static/js/main.c84d4191.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ json-server/public/precache-manifest.e6e381819383cc5e6019a61d371a07dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34347e06f847d446b9002407e45f7308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6e7cfb2b5afdfe2827b",
+    "url": "/static/css/2.c21e1fb8.chunk.css"
+  },
+  {
+    "revision": "34669d1f98fc2233686f",
+    "url": "/static/css/main.8d389e83.chunk.css"
+  },
+  {
+    "revision": "c6e7cfb2b5afdfe2827b",
+    "url": "/static/js/2.18cf85a8.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.18cf85a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34669d1f98fc2233686f",
+    "url": "/static/js/main.34bec54f.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
