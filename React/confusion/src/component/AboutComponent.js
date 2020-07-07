43bdef80bc7940a8279aebe9a33fa42174472621
Meet Doshi <meetdoshi193@gmail.com>
2020-07-07 20:03:06 +0530

    Assignment 4

--- React/confusion/src/component/AboutComponent.js
+++ React/confusion/src/component/AboutComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger } from 'react-animation-components';
 
 function RenderLeader({leader}) {
     return(
         <Media tag="li">
             <Media left middle>
-                <Media object src={leader.image} alt={leader.name} />
+                <Media object src={baseUrl + leader.image} alt={leader.name} />
             </Media>
             <Media body className="ml-5">
                 <Media heading>{leader.name}</Media>
@@ -19,9 +21,11 @@ function RenderLeader({leader}) {
 
 function About(props) {
 
-    const leaders = props.leaders.map((leader) => {
+    const leaders = props.leaders.leaders.map((leader) => {
         return (
-            <RenderLeader leader={leader} />
+            <Fade in>
+                <RenderLeader leader={leader} />
+            </Fade>
         );
     });
 
@@ -81,7 +85,9 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list>
-                        {leaders}
+                        <Stagger in>
+                            {leaders}
+                        </Stagger>
                     </Media>
                 </div>
             </div>
--- React/confusion/src/redux/leaders.js
+++ React/confusion/src/redux/leaders.js
@@ -1,31 +1,21 @@
-import { LEADERS } from '../shared/leaders';
+import * as ActionTypes from './ActionTypes';
 
-export const Leaders = (state = LEADERS, action) => {
-    switch(action){
+export const Leaders = (state  = { 
+        isLoading: true,
+        errMess: null,
+        leaders: []
+    }, action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_LEADERS:
+            return {...state, isLoading: false, errMess: null, leaders: action.payload};
+
+        case ActionTypes.LEADERS_LOADING:
+            return {...state, isLoading: true, errMess: null, leaders: []}
+
+        case ActionTypes.LEADERS_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
+
         default:
             return state;
     }
 };
-
-// change it for leaders
-// import * as ActionTypes from './ActionTypes';
-
-// export const Promotions = (state  = { 
-//         isLoading: true,
-//         errMess: null,
-//         promotions:[]
-//     }, action) => {
-//     switch (action.type) {
-//         case ActionTypes.ADD_PROMOS:
-//             return {...state, isLoading: false, errMess: null, promotions: action.payload};
-
-//         case ActionTypes.PROMOS_LOADING:
-//             return {...state, isLoading: true, errMess: null, promotions: []}
-
-//         case ActionTypes.PROMOS_FAILED:
-//             return {...state, isLoading: false, errMess: action.payload};
-
-//         default:
-//             return state;
-//     }
-// };
